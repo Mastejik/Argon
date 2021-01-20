cacae7c335f1bfd10a1041089cb0428ce525c6df
Mastej <63288930+Mastejik@users.noreply.github.com>
2021-01-20 12:26:01 +0100

    portfolio commit

---
+++ Assets/Custom Code/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Tooltip("Use prefab")] [SerializeField] GameObject EnemyDeathFX;
+    [Tooltip("Use prefab")] [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 100;
+    [SerializeField] int hitPoints = 10;
+
+
+    ScoreBoard scoreBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider BoxCollider = gameObject.AddComponent<BoxCollider>();
+        BoxCollider.isTrigger = false;
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit() // do some FX
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        hitPoints = hitPoints - 1;
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(EnemyDeathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Custom Code/PlayerControler.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerControler : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In ms-1")] [SerializeField] float ControlSpeed = 50f;
+    [Tooltip("In m")] [SerializeField] float xRange = 24f;
+    [Tooltip("In m")] [SerializeField] float yRange = 18f;
+    [SerializeField] GameObject[] guns;
+    
+    [Header("Screen-Position Based")]
+    [SerializeField] float positionRollFactor = 1f;
+    [SerializeField] float positionPitchFactor = -1.5f;
+    [SerializeField] float positionYawFactor = 2.5f;
+    
+    [Header("Control-Throw Based")]
+    [SerializeField] float controlPitchFactor = 1f;
+    [SerializeField] float controlYawFactor = 1f;
+    [SerializeField] float controlRollFactor = 0f;
+
+
+    float xThrow;
+    float yThrow;
+    float zThrow;
+    bool isControlEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+            ProcessFiring();
+        }
+    }
+
+    void OnPlayerDeath()
+    {
+        isControlEnabled = false;
+    }
+    private void ProcessTranslation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * ControlSpeed * Time.deltaTime;
+        float yOffset = yThrow * ControlSpeed * Time.deltaTime;
+
+        float rawNewX = transform.localPosition.x + xOffset;
+        float rawNewY = transform.localPosition.y + yOffset;
+
+        float clampedX = Mathf.Clamp(rawNewX, -xRange, xRange);
+        float clampedY = Mathf.Clamp(rawNewY, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedX, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedY, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float zThrow = 1;
+
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor + xThrow * controlYawFactor;
+        float roll = transform.localPosition.z * positionRollFactor * zThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessFiring()
+    {
+        if (CrossPlatformInputManager.GetButton("Fire"))
+        {
+            SetGunsActive(true);
+        }
+        else
+        {
+            SetGunsActive(false);
+        }
+    }
+    private void SetGunsActive(bool isActive)
+    {
+        foreach (GameObject gun in guns) // may affect death FX
+        {
+            var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+        }
+    }
+
+    
+}
+     
+    
+
